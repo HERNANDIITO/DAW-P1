219c0699f0c54b81b51dd03789ac37033e44002b
HERNANDIITO <pablohhgg@gmail.com>
2024-10-24 11:13:58 +0200

    modal y formula, falta tabla

--- js/brochureRequest.js
+++ js/brochureRequest.js
@@ -4,44 +4,95 @@
 // Historial de cambios:
 // 23/10/2024 - Creado
 
+var modal;
+
 function init() {
-    document.getElementById("price").innerText = 10 + "€"
+    document.getElementById("price").innerText = '10.00 ' + "€"
+    modal = document.getElementById("details")
+    generateTable()
+}
+
+function generateTable() {
+    const table = document.getElementById("price-table")
+    table.insertRow()
+
+    table.insertCell()
+    table.insertCell()
+
+    table.insertCell().innerText = "Blanco y negro"
+    table.insertCell().innerText = "Color"
+
+    table.insertRow()
+
+    table.insertCell().innerText = "Número de páginas"
+    table.insertCell().innerText = "Número de fotos"
+    table.insertCell().innerText = "150 - 300 dpi"
+    table.insertCell().innerText = "450 - 900 dpi"
+    table.insertCell().innerText = "150 - 300 dpi"
+    table.insertCell().innerText = "450 - 900 dpi"
+
+    for (let index = 0; index <= 15; index++) {
+        table.insertRow(index)
+    }
+    
+    table.appendChild()
+}
+
+function showModal() {
+    modal.showModal()
+}
+
+function closeModal() {
+    modal.close()
 }
 
 function getPrecioCopias(copias) {
-
-    // Hagan esto 
+    precio = 0;
+    for (let copiasLeft = copias; copiasLeft > 0; copiasLeft--) {
+        if ( copiasLeft < 5 ) { precio += 2 }
+        else if ( copiasLeft >= 5 && copiasLeft <= 10 ) { precio += 1.8 }
+        else if ( copiasLeft > 10 ) { precio += 1.6 }
+        console.log("copiasLeft", copiasLeft);
+        console.log("precio", precio);
+        
+    }
 
     return precio;
 }
 
-function updatePrice() {
+function getPrecio(numCopias, numFotos, resFotos, color) {
 
     var precioTotal = 0;
+
     const base = 10;
-    const numCopias = document.getElementById("numCopias").value;
-    const numFotos  = document.getElementById("numFotos").value;
-    const resFotos  = document.getElementById("resFotos").value;
-    const color     = document.getElementById("color").checked;
-    
     const precioFotos   = resFotos <= 300 ? 0 : 0.2
     const precioCopias  = getPrecioCopias(numCopias);
     const precioColor   = color ? 0.5 : 0 
 
-    console.log("numCopias",numCopias);
-    console.log("numFotos ",numFotos );
-    console.log("resFotos ",resFotos );
-    console.log("color    ",color    );
-    console.log("precioFotos ",precioFotos );
-    console.log("precioCopias",precioCopias);
-    console.log("precioColor ",precioColor );
-    
-
-    const precioTotalPaginas = numCopias * precioCopias
     const precioTotalFotos = numFotos * ( precioFotos + precioColor )
     
-    precioTotal = base + precioTotalPaginas + precioTotalFotos 
+    precioTotal = base + precioCopias + precioTotalFotos
 
-    document.getElementById("price").innerText = precioTotal + "€"
+    precioTotal = precioTotal.toString().split(".")
+    var entero = precioTotal[0]
+    var decimal = precioTotal[1]
+
+    if ( !decimal ) { decimal = "00" }
+    else {
+        if ( decimal.length == 1 ) { decimal += '0' }
+        if ( decimal.length > 2 ) { decimal = decimal.substring(0, 2) }
+    }
+
+    return `${entero}.${decimal} €`;
+}
+
+function updatePrice() {
+
+    const numCopias = document.getElementById("numCopias").value;
+    const numFotos  = document.getElementById("numFotos").value;
+    const resFotos  = document.getElementById("resFotos").value;
+    const color     = document.getElementById("color").checked;
+
+    document.getElementById("price").innerText = getPrecio(numCopias, numFotos, resFotos, color)
     
 }
