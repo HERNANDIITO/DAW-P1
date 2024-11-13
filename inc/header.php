f9c0fea44aa282ecaa3e48dbb5c2ed5dbb48f168
HERNANDIITO <pablohhgg@gmail.com>
2024-11-13 20:20:29 +0100

    buenas noches pepito

--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,26 @@
+<?php
+
+    function getMessage(): string {
+        $returnValue = "";
+
+        date_default_timezone_set('Europe/Madrid');
+        $hora = date('H:i');
+
+        if ($hora >= "06:00" && $hora <= "11:59") {
+            $returnValue = "Buenos días Pepito";
+        } elseif ($hora >= "12:00" && $hora <= "15:59") {
+            $returnValue = "Hola Pepito";
+        } elseif ($hora >= "16:00" && $hora <= "19:59") {
+            $returnValue = "Buenas tardes Pepito";
+        } else {
+            $returnValue = "Buenas noches Pepito";
+        }
+
+        return $returnValue;
+    }
+
+?>
+
 <?php  if ( false )  { ?>
 
     <header class="mainHeader">
@@ -27,8 +50,11 @@
             <section class="profile">
                 <a class="navLink" href="/private/myProfile.php"> <i class="fa-solid fa-user"></i> <span>Perfil</span> </a>
                 <a class="navLink" href="/index.php"> <i class="fa-solid fa-right-to-bracket"></i> <span>Cerrar sesión</span> </a>
-            </section>
+            </section>            
         </nav>
+        <section class="welcomeMessage">
+            <p><?php echo getMessage() ?></p>
+        </section>
     </header>
     
 <?php } ?>
